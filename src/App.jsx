9dff3ab2ca9dbead2289996b4a4601eb985d5aa4
Emanuel Martinez <yinproducciones@gmail.com>
2023-12-03 13:03:32 -0300

    adding activities component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TopBar from './components/TopBar'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Activities from './components/Activities'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <TopBar />
       <Navbar />
       <Hero />
+      <Activities />
     </>
   )
 }
