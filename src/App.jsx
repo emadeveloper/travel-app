1878567604de14ab228f08e17ad18d1962d39023
Emanuel Martinez <yinproducciones@gmail.com>
2023-12-03 12:18:05 -0300

    adding navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TopBar from './components/TopBar'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <TopBar />
+      <Navbar />
     </>
   )
 }
